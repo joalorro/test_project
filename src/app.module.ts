8a0e4fdc262e267f788572bd7551a442455af03b
josue <joalorro@gmail.com>
2023-03-08 15:14:01 -0500

    bootstrapped nest project, added loggermiddleware to app module

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { LoggerMiddleware } from 'common/logger.middleware';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer  
+      .apply(LoggerMiddleware)
+      .forRoutes("*")
+  }
+}
