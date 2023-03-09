d8fd5089de72245567758ad33691dd86d75af122
josue <joalorro@gmail.com>
2023-03-09 12:04:03 -0500

    add swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Example API')
+    .setDescription('Demo API')
+    .setVersion('1.0')
+    .addTag('examples')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3001);
 }
 bootstrap();
