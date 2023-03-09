1d0e5a90394fbde8949bdc14b2293a62c41788ce
josue <joalorro@gmail.com>
2023-03-09 11:57:46 -0500

    add exceptions filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule, Scope } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
 import { ExampleModule } from './examples/examples.module';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
 @Module({
   imports: [ExampleModule],
@@ -15,6 +16,10 @@ import { ExampleModule } from './examples/examples.module';
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
       scope: Scope.REQUEST,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
     },
   ],
 })
---
+++ src/filters/http-exception.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    const responseBody = {
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    };
+    this.logger.error(responseBody);
+    response.status(status).json(responseBody);
+  }
+}
